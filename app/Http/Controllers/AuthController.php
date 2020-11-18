055ebdff9448edcfa44512bea50eea796b443d85
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2020-11-18 23:54:19 +0400

    finshed auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+  public function customLogin(Request $request)
+  {
+    $user = User::where('email', $request->email)->first();
+    if (!$user) {
+      return abort(401);
+    }
+
+    if (!Hash::check($request->password, $user->password)) {
+      return abort(401);
+    }
+
+    Auth::login($user);
+
+    return redirect()->route('home');
+  }
+
+  public function customLogout(Request $request)
+  {
+    Auth::logout();
+
+    return redirect()->route('login');
+  }
+
+  public function customRegister(Request $request)
+  {
+    $request->validate([
+      'name' => ['required', 'string', 'min:10', 'max:100'],
+      'email' => ['required', 'email', 'max:100'],
+      'password' => ['required', 'min:5', 'max:25', 'confirmed'],
+    ]);
+
+    User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => bcrypt($request->password)
+    ]);
+
+    return redirect()->route('login');
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/product', '\App\Http\Controllers\ProductController@index')->name('products.all');
-Route::get('/product/editsomeproduct', '\App\Http\Controllers\ProductController@edit')->name('products.edit');
-Route::post('/product/update', '\App\Http\Controllers\ProductController@update')->name('products.update');
-Route::post('/product/delete', '\App\Http\Controllers\ProductController@delete')->name('products.delete');
-Route::post('/product/store', '\App\Http\Controllers\ProductController@store')->name('products.store');
+Route::get('/product', [ProductController::class, 'index'])->name('products.all');
+
+Route::get('/product/editsomeproduct', [ProductController::class, 'edit'])->name('products.edit');
+Route::post('/product/update', [ProductController::class, 'update'])->name('products.update');
+Route::post('/product/delete', [ProductController::class, 'delete'])->name('products.delete');
+Route::post('/product/store', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::post('/custom/login', [AuthController::class, 'customLogin'])->name('custom.login');
+Route::post('/custom/register', [AuthController::class, 'customRegister'])->name('custom.register');
+Route::post('/custom/logout', [AuthController::class, 'customLogout'])->name('custom.logout');
+
+Auth::routes();
