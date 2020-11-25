d141402a60f5410ee838d1619a77a7cd9f5fd4ae
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2020-11-25 21:05:36 +0400

    added routes

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,34 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/product', [ProductController::class, 'index'])->name('products.all');
+Route::middleware('custom_auth')->name('test.')->prefix('/app/user')->group(function () {
+  Route::get('test', [ProductController::class, 'index'])->middleware('custom_guest');
+  Route::get('test2', [ProductController::class, 'index'])->name('routes.2');
+  Route::get('test3', [ProductController::class, 'index'])->name('routes.3');
+});
 
-Route::get('/product/editsomeproduct', [ProductController::class, 'edit'])->name('products.edit');
-Route::post('/product/update', [ProductController::class, 'update'])->name('products.update');
-Route::post('/product/delete', [ProductController::class, 'delete'])->name('products.delete');
-Route::post('/product/store', [ProductController::class, 'store'])->name('products.store');
+Route::middleware('custom_auth')->group(function () {
+  Route::prefix('product')
+    ->name('products.')
+    ->group(function () {
+      Route::get('/', [ProductController::class, 'index'])->name('all');
+      Route::get('/editsomeproduct', [ProductController::class, 'edit'])->name('edit');
+      Route::post('/update', [ProductController::class, 'update'])->name('update');
+      Route::post('/delete', [ProductController::class, 'delete'])->name('delete');
+      Route::post('/store', [ProductController::class, 'store'])->name('store');
+    });
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+  Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
 
-Route::post('/custom/login', [AuthController::class, 'customLogin'])->name('custom.login');
-Route::post('/custom/register', [AuthController::class, 'customRegister'])->name('custom.register');
-Route::post('/custom/logout', [AuthController::class, 'customLogout'])->name('custom.logout');
+
+
+Route::prefix('custom')
+  ->name('custom.')
+  ->group(function () {
+    Route::post('/login', [AuthController::class, 'customLogin'])->name('login')->middleware('custom_guest');
+    Route::post('/register', [AuthController::class, 'customRegister'])->name('register')->middleware('custom_guest');
+    Route::post('/logout', [AuthController::class, 'customLogout'])->name('logout')->middleware('custom_auth');
+  });
 
 Auth::routes();
